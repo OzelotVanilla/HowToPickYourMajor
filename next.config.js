bf600b1cd2079f950126c88ed3dcb22cea950894
Ozelot Vanilla <shuihenyun@qq.com>
2023-09-20 09:38:19 +0800

    init

---
+++ next.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    pageExtensions: ["tsx"],
+    /**
+     * Disable module CSS, 
+     *  from "https://stackoverflow.com/questions/67934463/how-to-turn-off-css-module-feature-in-next-js".
+     */
+    webpack(config)
+    {
+        config.module.rules.forEach(
+            (rule) =>
+            {
+                const { oneOf } = rule;
+                if (oneOf)
+                {
+                    oneOf.forEach((one) =>
+                    {
+                        if (!`${one.issuer?.and}`.includes('_app')) return;
+                        one.issuer.and = [path.resolve(__dirname)];
+                    });
+                }
+            }
+        )
+        return config;
+    },
+}
+
+module.exports = nextConfig
---
+++ readme.md
@@ -0,0 +1,16 @@
+How To Pick Your Major
+====
+
+Basic `.gitignore`
+----
+
+To avoid the conflicts of `.gitignore`, this project suggest you to create your own one.
+
+Below is the basic content of `.gitignore`:
+
+```
+.gitignore
+
+node_modules/
+.next/
+```
