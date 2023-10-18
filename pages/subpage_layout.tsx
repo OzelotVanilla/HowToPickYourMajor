08a95f86abf7f0b40c6a7c66d2483506c24df8e0
Ozelot Vanilla <shuihenyun@qq.com>
2023-10-18 08:51:36 +0800

    feature - Language Button on Subpage Layout

--- pages/subpage_layout.tsx
+++ pages/subpage_layout.tsx
@@ -2,6 +2,7 @@ import { ConfigProvider, Layout, Typography } from "antd"
 import "./subpage_layout.scss"
 import { useI18N } from "@/i18n/i18n"
 import Link from "next/link"
+import { LanguageSelect_Button } from "@/components/LanguageSelect_Button"
 const { Header, Content } = Layout
 const { Title } = Typography
 
@@ -18,6 +19,7 @@ export default function SubPage_Layout({ children }: React.PropsWithChildren)
                         <span style={{ color: "#000000" }}>{text.navbar.project_name}</span>
                     </Link></Title>
                 </div>
+                <div id="SubPage_Layout_Header_LanguageButton"><LanguageSelect_Button /></div>
             </Header>
             <Content id="SubPage_Layout_Content">{children}</Content>
         </div>
